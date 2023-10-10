31dc6471e00a84792e8bc57e8d4c238433aa869f
Eunice Mwenda <eunice.mwenda@student.moringaschool.com>
2023-10-10 16:11:19 +0300

    List crud lab

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,32 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return [1, 2, 3, 4]
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(create_a_list, element):
+    create_a_list.append(5)
+    return create_a_list
+    
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(create_a_list, element):
+    create_a_list.insert(0, element) 
+    return create_a_list
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(create_a_list):
+    create_a_list.pop()
+    return create_a_list
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(create_a_list):
+    del create_a_list [0]
+    return create_a_list
+    
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(create_a_list):
+    return create_a_list [0] 
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_element_from_index(create_a_list, index):
+    return create_a_list [index]
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(create_a_list):
+    return create_a_list [-1]
